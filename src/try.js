93300f8d34e224f71745951c04059b05a0ddbcd5
skillzaa <skillzaa.com@gmail.com>
2022-11-08 01:54:45 +0500

    inthe name of Allah

--- src/try.js
+++ src/try.js
@@ -1,7 +1,8 @@
 import GlUtil from "./core/glUtil.js";
 import AssetsCollection from "./assets/assetsCollection.js";
 const gl = GlUtil.getGl("bilza");
-const asset = new AssetsCollection.Sq2X2(5, 50, 40, 40);
+const asset = new AssetsCollection.Sq2X2(0, 0, 100, 100);
+asset.palette.color[1].set(0.4, 0, 0.3);
 const comp = asset.getAsset();
 GlUtil.clear(gl, 0.2, 0.2, 0.2);
 comp.init(gl);
--- typescript/try.ts
+++ typescript/try.ts
@@ -1,18 +1,12 @@
-import getClrObj  from "./color/getClrObj.js";
 import GlUtil from "./core/glUtil.js";
-////////////////////////////////////////////////
-import TriComp from "./core/triComp.js";
-import upload515ToTriCont from "./formats/upload515ToTriCont.js";
-import Primtive from "./primtives/primitives.js";
 import AssetsCollection from "./assets/assetsCollection.js";
-////////////////////////////////////////////////
 ////////////////////////////////////////////////
 
 const gl = GlUtil.getGl("bilza");
 //////////////////////////////////////
 
-const asset = new AssetsCollection.Sq2X2( 5 , 50 , 40, 40);
-
+const asset = new AssetsCollection.Sq2X2( 0 , 0 , 100, 100);
+asset.palette.color[1].set(0.4,0,0.3);
 const comp = asset.getAsset();
 
 GlUtil.clear(gl,0.2,0.2,0.2);

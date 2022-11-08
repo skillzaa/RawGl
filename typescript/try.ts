fe7f305e78844d604886fc9ccdc549f7b0658d6a
skillzaa <skillzaa.com@gmail.com>
2022-11-08 18:38:38 +0500

    100 rect example

--- typescript/try.ts
+++ typescript/try.ts
@@ -1,20 +1,34 @@
-import { TriCont,GlUtil } from "./rawGl.js";
+import {GlUtil,TriCont} from "../src/rawGl.js";
 ////////////////////////////////////////////////
+
 const gl = GlUtil.getGl("bilza");
+
 //////////////////////////////////////
-const comp = new TriCont( 10 , 10 , 80, 80);
-//---Mark the Background
-comp.add.rectWH(0,0,100,100,0,0,1);
+/*
+This function will draw a grid of 10 X 10 squares.
+*/
+function grid(){
+/**
+ * Try changing the x,y, width and height of TriCont
+ */    
+const comp = new TriCont(10,10,80,80);
+//-------------------
+let y = 0;
+for (let outer = 0; outer < 10; outer++) {
 
-comp.add.tri( 0,0,    50,0,    50,50,  1);
-comp.add.tri( 0,100,  0,50,    50,50,  1,1);
-
-comp.add.polygon(80,80,  10,10, 120,  1,0,0);
-comp.add.polygon(80,60,  10,10, 6,    1,1,0);
-comp.add.polygon(80,40,  10,10, 4,    0,1,0);
-comp.add.polygon(80,20,  10,10, 3,    0,1,1);
-
+    for (let inner = 0; inner < 10; inner++) {
+        comp.add.rectWH(
+            inner * 10 ,y,  // location x and y
+            10,10,      // width and height
+        Math.random(),Math.random(),Math.random() );
+    }
+y += 10;    
+}    
+GlUtil.clear(gl,0.3,0.2,0.2);
 comp.init(gl);
-/////////////////////////////////////
-GlUtil.clear(gl,0.2,0.1,0.1);
 comp.draw(gl);
+}
+////////////////////////////////////////
+setInterval(function(){
+    grid();
+}, 1000);
